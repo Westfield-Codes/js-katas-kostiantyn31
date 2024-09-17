1d3df3fe01fc1e521dfc04eca5cc3ac47e55b3b2
Kostaintyn <kostiantyn208148@student-schoolsofwestfield.org>
2024-09-17 12:07:52 -0400

    color kata

--- done.js
+++ done.js
@@ -4,6 +4,28 @@
 
 /* No Prompt */
 
+/* Variable and Alert Katas */
+// These katas do not involve functions and are very basic. 
+
+/* COLOR KATAS */
+
+/* Var Alert */
+// make a string variable for a color, set it equal to your favorite
+var color ="black";
+// alert the message" "My favorite color is " plus the color
+alert("My favorite color is " + color);
+
+
+/* Var Alert Prompt */
+// make a string variable for color, prompt the user for their favorite
+color = prompt(What is your favorite color?);
+// alert color " is a nice color!"
+alert(color + "is a nice color!")
+
+/* Var Alert Prompt Conditional */
+// make a variable for color, prompt the user for their favorite
+// if user enters "black" alert "Black is not really a color." 
+// otherwise alert color " is a nice color!"
 
 /* Prompt */
 
